b551d4c52bf0b01c63d7ef9ba2bdd7d15725b907
Nguyễn Trọng <vantrong2007vn@gmail.com>
2024-02-16 00:05:40 +0700

    fixed orther player in level has same your own color

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,18 @@ using namespace geode::prelude;
 class $modify(PlayerObject) {
 	void setColor(ccColor3B const& p0) {
 		auto gm = GameManager::sharedState();
-		PlayerObject::setColor((gm->getPlayLayer() || gm->getEditorLayer()) ? gm->colorForIdx(gm->getPlayerColor()) : p0);
+		GJBaseGameLayer* layer = nullptr;
+		if (gm->getPlayLayer()) layer = gm->getPlayLayer();
+		else if (gm->getEditorLayer()) layer = gm->getEditorLayer();
+		if ((layer != nullptr) && (layer->m_player2 == this)) PlayerObject::setColor(gm->colorForIdx(gm->getPlayerColor()));
+		else PlayerObject::setColor(p0);
 	}
 	void setSecondColor(ccColor3B const& p0) {
 		auto gm = GameManager::sharedState();
-		PlayerObject::setSecondColor((gm->getPlayLayer() || gm->getEditorLayer()) ? gm->colorForIdx(gm->getPlayerColor2()) : p0);
+		GJBaseGameLayer* layer = nullptr;
+		if (gm->getPlayLayer()) layer = gm->getPlayLayer();
+		else if (gm->getEditorLayer()) layer = gm->getEditorLayer();
+		if ((layer != nullptr) && (layer->m_player2 == this)) PlayerObject::setSecondColor(gm->colorForIdx(gm->getPlayerColor2()));
+		else PlayerObject::setSecondColor(p0);
 	}
 };
